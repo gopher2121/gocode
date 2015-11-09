4ccfa2dd2e5b1f2cc980b863d249552df695ef6b
Dhiraj <dhirajdas.666@gmail.com>
2015-11-10 00:40:07 +0530

    constants(string bool and number) in golang

---
+++ gofundamentalsforwebprogramming/constants.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+const PI = 3.142
+const (
+	gopherstring = "dhiraj"
+	gopherbool   = true
+	gophernum    = 673679
+)
+
+func main() {
+	fmt.Println("PI, Gopher string, gopher bool, gophernum \n", PI, gopherstring, gopherbool, gophernum)
+}
